6588a44bb59535498eff720e421eaa6e6e26ff93
Alice Pote <alice.writes.wrongs@gmail.com>
2015-12-28 10:47:06 -0500

    renamed the nav container

--- src/simone.js
+++ src/simone.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { render } from 'react-dom';
 
 import { Router, Route } from 'react-router';
-import Nav from 'view_controllers/navigation';
+import SimoneNav from 'view_controllers/navigation';
 
 // this is the router for the whole application
 render((
   <Router>
-    <Route path="/" component={Nav}>
+    <Route path="/" component={SimoneNav}>
     </Route>
   </Router>
 ), document.getElementById('simone'));
