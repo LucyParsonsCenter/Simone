310920293579dbab295cf2a45c2589194da284a9
Alice Pote <alice.writes.wrongs@gmail.com>
2015-12-28 10:55:17 -0500

    adding a route for the register

--- src/view_controllers/navigation.js
+++ src/view_controllers/navigation.js
@@ -1,32 +1,27 @@
 import React from 'react';
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 
-// class Nav extends React.Component {
-//   render () {
-//     return (
-//       <div>
-//         <h1>Hey!</h1>
-//       </div>
-//     );
-//   }
-// }
-
-
-class Nav extends React.Component {
+class SimoneNav extends React.Component {
   render() {
     return (
-      <Nav bsStyle="tabs" activeKey={1} onSelect={this.handleSelect}>
-        <NavItem eventKey={1} href="/">Home</NavItem>
-        <NavItem eventKey={2} title="Item">NavItem 2 content</NavItem>
-        <NavItem eventKey={3} disabled>NavItem 3 content</NavItem>
-        <NavDropdown eventKey={4} title="Dropdown" id="nav-dropdown">
-          <MenuItem eventKey="4.1">Action</MenuItem>
-          <MenuItem eventKey="4.2">Another action</MenuItem>
-          <MenuItem eventKey="4.3">Something else here</MenuItem>
-          <MenuItem eventKey="4.4">Separated link</MenuItem>
-        </NavDropdown>
-      </Nav>
+      <div>
+        <Navbar>
+          <Navbar.Header>
+          </Navbar.Header>
+          <Nav>
+            <LinkContainer to="/">
+              <NavItem>Home</NavItem>
+            </LinkContainer>
+            <LinkContainer to="/sales">
+              <NavItem>Sales</NavItem>
+            </LinkContainer>
+          </Nav>
+        </Navbar>
+        {this.props.children}
+      </div>
     );
   }
 }
 
-export default Nav;
+export default SimoneNav;
