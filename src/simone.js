7ea09854686414d42a04d11598d3a644bdba2bbf
Alice Pote <alice.writes.wrongs@gmail.com>
2015-12-28 10:58:09 -0500

    import and add routes!

--- src/simone.js
+++ src/simone.js
@@ -3,11 +3,15 @@ import { render } from 'react-dom';
 
 import { Router, Route } from 'react-router';
 import SimoneNav from 'view_controllers/navigation';
+import Home from 'view_controllers/home';
+import Register from 'view_controllers/register';
 
 // this is the router for the whole application
 render((
   <Router>
     <Route path="/" component={SimoneNav}>
+      <Route path="/home" component={Home} />
+      <Route path="/sales" component={Register} />
     </Route>
   </Router>
 ), document.getElementById('simone'));
