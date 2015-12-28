718983df89615ee0f0a9a2331df62d25ba1d6b18
Alice Pote <alice.writes.wrongs@gmail.com>
2015-12-28 00:21:47 -0500

    skeleton of a react router application

--- src/app.jsx
+++ src/app.jsx
@@ -1,2 +1,21 @@
 import React from 'react';
+import { render } from 'react-dom';
 
+import { Router, Route } from 'react-router';
+
+const App = React.createClass({
+  render () {
+    return (
+      <div>
+        <h1>Hey!</h1>
+      </div>
+    )
+  }
+})
+
+render((
+  <Router>
+    <Route path="/" component={App}>
+    </Route>
+  </Router>
+), document.body)
